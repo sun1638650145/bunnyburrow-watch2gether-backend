612e633072106f3345dd53ba9ffa4c02d0d5f41a
sun1638650145 <1638650145@qq.com>
2023-01-20 19:31:01 +0800

    Update service module.

--- watch2gether/core/service.py
+++ watch2gether/core/service.py
@@ -1,46 +1,59 @@
-from typing import Generator
+import os
 
-import m3u8
-import uvicorn
+from pathlib import Path
+from typing import Union
 
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse, RedirectResponse
+from uvicorn import run
 
 from watch2gether import __version__ as w2g_version
 
 app = FastAPI(version=w2g_version)
+video_directory = Path()  # 视频文件夹路径.
 
 
-@app.get('/video/{video_name}')
-def create_streaming(video_name: str) -> StreamingResponse:
-    """创建流媒体服务.
+@app.get('/video/{video_name}/')
+def redirect_streaming_wrapper(video_name: str) -> RedirectResponse:
+    """重定向视频流媒体.
 
     Args:
         video_name: str,
             视频名称(路径参数), 用于访问播放的流媒体视频.
+    """
+    return RedirectResponse(url=f'/file/{video_name}.m3u8',
+                            status_code=301)
+
+
+@app.get('/file/{file_name}')
+def create_vod_streaming(file_name: str) -> FileResponse:
+    """创建流媒体(点播)服务.
+
+    Args:
+        file_name: str,
+            m3u8文件名称(路径参数), 用于访问播放的流媒体视频的m3u8索引.
 
     Return:
-        m3u8视频流响应.
+        ts文件视频流.
     """
-    def _video_streamer() -> Generator:
-        # 解析m3u8文件, 加载播放列表.
-        playlist = m3u8.load(uri=f'./{video_name}/playlist.m3u8')
-        # 读取HLS视频流片段.
-        for segment in playlist.segments:
-            yield open(segment.absolute_uri, 'rb').read()
-
-    return StreamingResponse(content=_video_streamer(),
-                             media_type='application/x-mpegURL')
+    return FileResponse(path=os.path.join(Path(video_directory), file_name))
 
 
-def launch_streaming(host: str = '127.0.0.1',
+def launch_streaming(video_dir: Union[str, os.PathLike],
+                     host: str = '127.0.0.1',
                      port: int = 8000):
     """启动流媒体服务.
 
     Args:
+        video_dir: str or os.PathLike,
+            流媒体视频文件夹路径.
         host: str, default='127.0.0.1',
             主机地址.
         port: int, default=8000,
             端口号.
     """
-    uvicorn.run(app, host=host, port=port)
+    # 通过修改全局变量传递视频文件夹路径给流媒体媒体服务.
+    global video_directory
+    video_directory = video_dir
+
+    run(app, host=host, port=port)
