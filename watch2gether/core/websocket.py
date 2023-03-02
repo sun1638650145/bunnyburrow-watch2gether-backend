0aa9e13f168116b73359880b800b610d8923fe53
sun1638650145 <1638650145@qq.com>
2023-03-02 12:42:28 +0800

    Add log information.

--- watch2gether/core/websocket.py
+++ watch2gether/core/websocket.py
@@ -1,7 +1,15 @@
+from datetime import datetime
 from typing import List
 
 from fastapi import APIRouter
 from fastapi import WebSocket, WebSocketDisconnect
+
+from watch2gether import logger
+
+
+def _get_current_time() -> str:
+    """获取当前时间."""
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 class WebSocketConnectionManager(object):
@@ -25,6 +33,9 @@ class WebSocketConnectionManager(object):
         await websocket.accept()
         self.active_connections.append(websocket)
 
+        logger.info(f'{_get_current_time()}: 客户端'
+                    f'({websocket.client.host}:{websocket.client.port})连接成功.')
+
     def disconnect(self, websocket: WebSocket):
         """断开WebSocket客户端连接.
 
@@ -34,15 +45,23 @@ class WebSocketConnectionManager(object):
         """
         self.active_connections.remove(websocket)
 
-    async def broadcast(self, data: dict):
+        logger.info(f'{_get_current_time()}: 客户端'
+                    f'({websocket.client.host}:{websocket.client.port})断开连接.')
+
+    async def broadcast(self, websocket: WebSocket, data: dict):
         """对连接的WebSocket客户端进行广播(传输JSON数据).
 
         Args:
+            websocket: WebSocket,
+                广播数据来源的WebSocket客户端.
             data: dict,
                 广播的数据(JSON格式).
         """
         for connection in self.active_connections:
             await connection.send_json(data)
+
+        logger.info(f'{_get_current_time()}: 客户端'
+                    f'({websocket.client.host}:{websocket.client.port})广播数据.')
 
 
 router = APIRouter()
@@ -63,7 +82,6 @@ async def create_websocket_endpoint(websocket: WebSocket):
         while True:
             # 接收并转发(广播)数据.
             data = await websocket.receive_json()
-            await manager.broadcast(data)
-            # TODO(Steve): 配置服务器端的日志.
+            await manager.broadcast(websocket, data)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
