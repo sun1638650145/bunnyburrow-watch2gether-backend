7dad47f2bbd6f3efe2cf5d30329c162ee68a425c
sun1638650145 <1638650145@qq.com>
2023-10-27 15:40:12 +0800

    Using the brand new `Streaming API`.

--- watch2gether/core/streaming.py
+++ watch2gether/core/streaming.py
@@ -7,11 +7,11 @@ from fastapi import HTTPException
 from fastapi import Request
 from fastapi.responses import FileResponse, RedirectResponse
 
-from watch2gether import logger
-from watch2gether.core import get_current_time
+from watch2gether import experimental_logger as logger
+
 
 router = APIRouter()
-video_directory = Path()  # 视频文件夹路径.
+videos_directory = Path()  # 全部流媒体视频的文件夹.
 
 
 @router.get('/video/{video_name}/')
@@ -23,38 +23,41 @@ async def redirect_streaming_wrapper(video_name: str) -> RedirectResponse:
             视频名称(路径参数), 用于访问播放的流媒体视频.
 
     Return:
-        HTTP重定向到`/file/{video_name}.m3u8`.
+        HTTP重定向到`/videos/{video_name}/{video_name}.m3u8`.
     """
-    return RedirectResponse(url=f'/file/{video_name}.m3u8',
+    return RedirectResponse(url=f'/videos/{video_name}/{video_name}.m3u8',
                             status_code=301)
 
 
-@router.get('/file/{file_name}')
-async def create_vod_streaming(request: Request,
-                               file_name: str) -> FileResponse:
+@router.get('/videos/{video_directory}/{file_name}')
+async def create_vod_streaming_endpoint(request: Request,
+                                        video_directory: str,
+                                        file_name: str) -> FileResponse:
     """创建流媒体(点播)服务.
 
     Args:
         request: Request,
-            一个Request请求.
+            当前的`Request`请求.
+        video_directory: str,
+            流媒体视频m3u8索引文件和ts文件所处的文件夹(路径参数), 一般和视频同名.
         file_name: str,
-            m3u8文件名称(路径参数), 用于访问播放的流媒体视频的m3u8索引.
+            请求的文件名(路径参数), 一般只需要请求`视频名.m3u8`即可.
 
     Return:
-        ts文件视频流.
+        自动顺序返回流媒体视频流.
 
     Raises:
         HTTPException 404: 如果文件不存在, 则向客户端返回`404`错误.
     """
-    # TODO(Steve): 通过修改全局变量传递视频文件夹路径给流媒体媒体服务, 耦合较高.
-    file_path = os.path.join(Path(video_directory), file_name)
+    # TODO(Steve): 通过修改全局变量传递流媒体视频文件夹路径, 耦合较高.
+    file_path = os.path.join(videos_directory, video_directory, file_name)
     if not os.path.exists(file_path):
+        logger.warning(f'客户端({request.client.host}:{request.client.port})\n'
+                       f'请求不存在文件:{file_path} 响应状态码: 404.')
+
         raise HTTPException(status_code=404)
 
-    response = FileResponse(file_path)
+    logger.info(f'客户端({request.client.host}:{request.client.port})\n'
+                f'请求文件:{file_path} 响应状态码: 200.')
 
-    logger.info(f'{get_current_time()}: '
-                f'客户端({request.client.host}:{request.client.port})\n'
-                f'请求文件: {file_path}\t响应状态码: {response.status_code}.')
-
-    return response
+    return FileResponse(file_path)
