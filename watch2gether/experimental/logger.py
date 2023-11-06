f3dfab366dc754a07f78fad7fc9168ca3315a6a6
sun1638650145 <1638650145@qq.com>
2023-11-06 16:40:44 +0800

    Update experimental logger.

--- watch2gether/experimental/logger.py
+++ watch2gether/experimental/logger.py
@@ -5,35 +5,31 @@ from logging import FileHandler, StreamHandler
 
 
 class Logger(logging.Logger):
-    """日志记录器."""
-    def __init__(self,
-                 name: str,
-                 show_on_console: bool = True,
-                 save_to_file: bool = False):
+    """日志记录器.
+
+    Attributes:
+        file_handler: FileHandler,
+            文件处理器, 将日志保存到文件.
+        stream_handler: StreamHandler,
+            数据流处理器, 将日志输出到终端.
+    """
+    def __init__(self, name: str):
         """初始化日志记录器.
 
         Args:
             name: str,
                 日志记录器的名称.
-            show_on_console: bool, default=True,
-                是否将日志记录在终端显示.
-            save_to_file: bool, default=False,
-                是否将日志记录保存到文件.
         """
         super().__init__(name)
 
         formatter = Formatter(fmt='[%(levelname)s] %(asctime)s %(message)s',
                               datefmt='%Y-%m-%d %H:%M:%S')
 
-        if show_on_console:
-            stream_handler = StreamHandler()
-            stream_handler.setFormatter(formatter)
-            # 添加处理器.
-            self.addHandler(stream_handler)
+        self.file_handler = FileHandler(filename=name + '.log', encoding='UTF-8')  # noqa: E501
+        self.file_handler.setFormatter(formatter)
+        self.stream_handler = StreamHandler()
+        self.stream_handler.setFormatter(formatter)
 
-        if save_to_file:
-            file_handler = FileHandler(filename=name + '.log',
-                                       encoding='UTF-8')
-            file_handler.setFormatter(formatter)
-            # 添加处理器.
-            self.addHandler(file_handler)
+        # 添加处理器.
+        self.addHandler(self.file_handler)
+        self.addHandler(self.stream_handler)
