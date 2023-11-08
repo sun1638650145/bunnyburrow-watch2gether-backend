a3ae21881928fdfea5b8cd79eefbbeac8ab1ac29
sun1638650145 <1638650145@qq.com>
2023-11-08 17:03:07 +0800

    Added refactored `help` command.

--- watch2gether/experimental_cli.py
+++ watch2gether/experimental_cli.py
@@ -1,11 +1,36 @@
 import argparse
 import sys
 
-from typing import List
+from typing import IO, List, Optional
 
 from watch2gether.experimental_command_wrapper import (
+    help_command,
     version_command
 )
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    """命令行参数解析器(本地化错误消息)."""
+    def error(self, message: str):
+        """输出使用方法和错误消息.
+
+        Args:
+            message: str
+                错误消息.
+        """
+        message = message.replace('unrecognized arguments', '未识别的参数')
+
+        self.print_usage()
+        sys.stderr.write(f'错误: {message}\n')
+        sys.exit(2)
+
+    def print_usage(self, file: Optional[IO[str]] = None):
+        """输出使用方法.
+
+        Args:
+            file: 继承父类参数.
+        """
+        sys.stdout.write(f'使用方法: {self.usage}\n')
 
 
 def parse_args(args: List[str]) -> argparse.Namespace:
@@ -18,19 +43,27 @@ def parse_args(args: List[str]) -> argparse.Namespace:
     Return:
         (解析后)参数组成的容器.
     """
-    parser = argparse.ArgumentParser(usage='w2g-cli {version}',
-                                     add_help=False,
-                                     exit_on_error=False)
+    parser = ArgumentParser(usage='w2g-cli {version}',
+                            add_help=False,
+                            exit_on_error=False)
     subparsers = parser.add_subparsers(dest='command')
 
     if len(args) < 2:  # 没有参数.
+        help_command()
         sys.exit(2)
     else:
         args = args[1:]
 
+        # 帮助命令.
+        subparsers.add_parser('help',
+                              usage='w2g-cli help',
+                              add_help=False,
+                              description='获取帮助信息.')
+
         # 查看版本命令.
         subparsers.add_parser('version',
                               usage='w2g-cli version',
+                              add_help=False,
                               description='查看命令行工具版本.')
 
         return parser.parse_args(args)
@@ -41,7 +74,10 @@ def run():
     try:
         args = parse_args(sys.argv)
 
-        if args.command == 'version':
+        if args.command == 'help':
+            help_command()
+        elif args.command == 'version':
             version_command()
     except argparse.ArgumentError:
+        help_command()
         sys.exit(2)
--- watch2gether/experimental_command_wrapper.py
+++ watch2gether/experimental_command_wrapper.py
@@ -3,9 +3,25 @@ from logging import Formatter
 from watch2gether import __version__
 from watch2gether import experimental_logger as logger
 
-# 针对命令行模式对日志记录器进行自定义.
-logger.removeHandler(logger.file_handler)
+
+# 命令行工具对日志格式进行自定义.
 logger.stream_handler.setFormatter(Formatter(fmt='%(message)s'))
+
+
+def help_command():
+    """"""
+    help_msg = f"""一起看电影命令行工具 {__version__}
+
+Bunnyburrow Software Project(兔窝镇软件计划)
+Copyright 2023 Steve R. Sun. All rights reserved.
+--------------------------------------------------
+使用方法:
+  w2g-cli help
+    获取帮助信息.
+  w2g-cli version
+    查看命令行工具版本.  
+"""
+    logger.info(help_msg)
 
 
 def version_command():
