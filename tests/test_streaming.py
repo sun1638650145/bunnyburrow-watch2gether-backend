d6bb1b6ef2988db98f6e24b659b381ca1985a2fc
sun1638650145 <1638650145@qq.com>
2023-11-09 17:35:57 +0800

    Improve test coverage.

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -6,9 +6,12 @@ from fastapi.testclient import TestClient
 
 import watch2gether as w2g
 
+
 # 注意: 需要先进行`test_convert.py`的测试生成流媒体资源!
 #  这样可以重复利用资源, 进而降低测试时间.
 w2g.streaming.videos_directory = Path('./tests/assets/')
+# 添加文件处理器, 增加对logger的测试覆盖.
+w2g.experimental_logger.add_file_handler('./watch2gether.log')
 
 
 class TestStreaming(object):
