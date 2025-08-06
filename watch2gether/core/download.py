ba5bd688820f930d958d48113699762e72838fd2
sun1638650145 <1638650145@qq.com>
2025-08-06 17:11:02 +0800

    Add support for downloading m3u8 videos.

---
+++ watch2gether/core/download.py
@@ -0,0 +1,34 @@
+import os
+
+from pathlib import Path
+from typing import Union
+from urllib.parse import quote, unquote
+from urllib.request import urlretrieve
+
+import m3u8
+
+
+def download_m3u8(url: str, m3u8_directory: Union[str, os.PathLike]):
+    """解析并下载指定URL的m3u8流媒体视频文件到本地.
+
+    Args:
+        url: str,
+            m3u8流媒体视频的URL.
+        m3u8_directory: str or os.PathLike,
+            m3u8文件夹的保存路径.
+    """
+    # 将URL转换成百分号编码.
+    url = quote(url, safe='/:')
+
+    # 创建用于保存m3u8的文件夹.
+    os.makedirs(Path(m3u8_directory).absolute(), exist_ok=True)
+
+    playlist = m3u8.load(uri=url)
+
+    # 保存m3u8播放列表文件.
+    playlist.dump(filename=os.path.join(m3u8_directory, unquote(Path(url).name) + '.m3u8'))
+
+    for segment in playlist.segments:
+        segment_url = playlist.base_uri + segment.uri
+        # 下载对应的ts分片文件.
+        urlretrieve(url=segment_url, filename=os.path.join(m3u8_directory, segment.uri))
