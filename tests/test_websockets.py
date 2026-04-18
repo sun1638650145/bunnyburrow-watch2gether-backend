3ceafb4e5ecfaff6de654f4835782862bb8a1e78
sun1638650145 <1638650145@qq.com>
2026-04-18 16:01:19 +0800

    test(websockets): udpate tests for WebSocket connections with `room_id`

--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -15,13 +15,15 @@ class TestWebSockets(object):
         """测试使用相同客户端ID连接被拒."""
         client = TestClient(w2g.app)
 
-        with client.websocket_connect('/ws/2023/'):
-            assert len(manager.active_connections) == 1
+        with client.websocket_connect('/ws/r1/2023/'):
+            room = manager.room_connections.get('r1')
+
+            assert len(room) == 1
 
             # 使用相同客户端ID连接.
             with pytest.raises(WebSocketDisconnect):
-                with client.websocket_connect('/ws/2023/'):
-                    assert len(manager.active_connections) == 1  # noqa: E501 连接被拒且仍为1个活跃连接.
+                with client.websocket_connect('/ws/r1/2023/'):
+                    assert len(room) == 1  # 连接被拒且仍为1个活跃连接.
 
     @pytest.mark.asyncio
     async def test_broadcast(self):
@@ -31,10 +33,10 @@ class TestWebSockets(object):
         s_data_b = {'text': 'Hello, World!'}
         s_data_c = 'Not JSON!'
 
-        with (client_a.websocket_connect('/ws/1001/') as websocket_a,
-              client_b.websocket_connect('/ws/1002/') as websocket_b):
+        with (client_a.websocket_connect('/ws/r0/1001/') as websocket_a,
+              client_b.websocket_connect('/ws/r0/1002/') as websocket_b):
             # 模拟由客户端a(实际上由服务器)发起广播, 且对自身广播.
-            await manager.broadcast(s_data_a)
+            await manager.broadcast(s_data_a, 'r0')
             # 由客户端a发起广播.
             websocket_a.send_json({
                 'props': {'type': 'websocket.broadcast'},
@@ -48,7 +50,9 @@ class TestWebSockets(object):
             r_data_b1 = websocket_b.receive_json()
             r_data_b2 = websocket_b.receive_json()
 
-            assert len(manager.active_connections) == 2
+            room = manager.room_connections.get('r0')
+
+            assert len(room) == 2
             assert r_data_a == s_data_a
             assert r_data_b1 == s_data_a
             assert r_data_b2.get('data') == s_data_b
@@ -58,8 +62,8 @@ class TestWebSockets(object):
         client_a, client_b = TestClient(w2g.app), TestClient(w2g.app)
         s_data = {'msg': 'Hi!'}
 
-        with (client_a.websocket_connect('/ws/1001/') as websocket_a,
-              client_b.websocket_connect('/ws/1002/') as websocket_b):
+        with (client_a.websocket_connect('/ws/r0/1001/') as websocket_a,
+              client_b.websocket_connect('/ws/r0/1002/') as websocket_b):
             # 客户端a向客户端b发起单播.
             websocket_a.send_json({
                 'props': {
@@ -79,5 +83,7 @@ class TestWebSockets(object):
 
             r_data = websocket_b.receive_json()
 
-            assert len(manager.active_connections) == 2
+            room = manager.room_connections.get('r0')
+
+            assert len(room) == 2
             assert r_data.get('data') == s_data
