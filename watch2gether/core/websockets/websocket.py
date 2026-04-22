04e778597c9a09e96aefb9abe951a8669d2e6ca6
sun1638650145 <1638650145@qq.com>
2026-04-22 16:00:14 +0800

    fix(websockets): resolve all static analysis warnings in `create_websocket_endpoint`

--- watch2gether/core/websockets/websocket.py
+++ watch2gether/core/websockets/websocket.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from fastapi import WebSocket, WebSocketDisconnect
 
 from watch2gether import logger
+from watch2gether.core.networking import get_client_address
 from watch2gether.core.websockets import ConnectionManager
 
 
@@ -32,7 +33,7 @@ async def create_websocket_endpoint(room_id: str,
             reason=f'当前房间({room_id})内具有相同ID的客户端已存在, 连接被拒绝!'  # noqa: E501
         )
 
-        logger.warning(f'客户端({websocket.client.host}:{websocket.client.port})'
+        logger.warning(f'客户端({get_client_address(websocket)})'
                        f'试图以已存在客户端ID#{client_id}连接房间({room_id})被拒!')
     else:
         await manager.connect(room_id, client_id, websocket)
@@ -51,7 +52,7 @@ async def create_websocket_endpoint(room_id: str,
                     else:
                         await manager.broadcast(data, room_id, client_id)
                 except JSONDecodeError:
-                    logger.warning(f'客户端({websocket.client.host}:{websocket.client.port})'  # noqa: E501
+                    logger.warning(f'客户端({get_client_address(websocket)})'
                                    f'在房间({room_id})中发送无法解析的JSON数据!')
         except WebSocketDisconnect:
             manager.disconnect(room_id, client_id)
