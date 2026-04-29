d2a01b64c9232a3d70b463d1b14cc5ccc0f0f1ff
sun1638650145 <1638650145@qq.com>
2026-04-29 18:34:12 +0800

    test(websockets): refine websocket test cases

--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -7,83 +7,133 @@ from fastapi.testclient import TestClient
 import watch2gether as w2g
 
 
-manager = w2g.websocket.manager  # 获取WebSocket连接管理器.
+@pytest.fixture
+def client():
+    return TestClient(w2g.app)
+
+
+@pytest.fixture
+def manager():
+    return w2g.websocket.manager
 
 
 class TestWebSockets(object):
-    def test_connection_refused(self):
-        """测试使用相同客户端ID连接被拒."""
-        client = TestClient(w2g.app)
+    def test_allows_same_client_id_in_different_rooms(self, client, manager):
+        """测试允许在不同房间中使用相同客户端ID."""
+        room_r0_id, room_r1_id = 'r0', 'r1'
+        client_id = 2026
 
-        with client.websocket_connect('/ws/r1/2023/'):
-            room = manager.room_connections.get('r1')
+        with (client.websocket_connect(f'/ws/{room_r0_id}/{client_id}/'),
+              client.websocket_connect(f'/ws/{room_r1_id}/{client_id}/')):
+            room_r0 = manager.room_connections[room_r0_id]
+            room_r1 = manager.room_connections[room_r1_id]
 
-            assert len(room) == 1
+            assert set(room_r0) == {client_id}
+            assert set(room_r1) == {client_id}
 
-            # 使用相同客户端ID连接.
+    def test_rejects_duplicate_client_id(self, client, manager):
+        """测试在同一房间中使用相同客户端ID连接被拒."""
+        room_id, client_id = 'r0', 2026
+
+        with client.websocket_connect(f'/ws/{room_id}/{client_id}/'):
+            room = manager.room_connections[room_id]
+
+            assert set(room) == {client_id}
+
+            # 在同一房间中使用相同客户端ID连接.
             with pytest.raises(WebSocketDisconnect):
-                with client.websocket_connect('/ws/r1/2023/'):
-                    assert len(room) == 1  # 连接被拒且仍为1个活跃连接.
+                with client.websocket_connect(f'/ws/{room_id}/{client_id}/'):
+                    assert set(room) == {client_id}  # noqa: E501 连接被拒且仍为1个活跃连接.
+
+    def test_client_broadcast(self, client, manager):
+        """测试由客户端发起广播."""
+        room_id = 'r0'
+        sender_client_id = 2026
+        receiver_a_client_id, receiver_b_client_id = 2024, 2025
+
+        data = {'text': 'Hello, World!'}
+
+        with (
+            client.websocket_connect(f'/ws/{room_id}/{sender_client_id}/') as sender_websocket,  # noqa: E501
+            client.websocket_connect(f'/ws/{room_id}/{receiver_a_client_id}/') as receiver_a_websocket,  # noqa: E501
+            client.websocket_connect(f'/ws/{room_id}/{receiver_b_client_id}/') as receiver_b_websocket  # noqa: E501
+        ):
+            room = manager.room_connections[room_id]
+
+            # 发送方客户端发送无法解析的广播数据.
+            sender_websocket.send_text('Hello, World!')
+            # 发送方客户端再次发起广播.
+            sender_websocket.send_json({
+                'props': {
+                    'type': 'websocket.broadcast'
+                },
+                'data': data
+            })
+
+            assert len(room) == 3
+            assert receiver_a_websocket.receive_json()['data'] == data
+            assert receiver_b_websocket.receive_json()['data'] == data
 
     @pytest.mark.asyncio
-    async def test_broadcast(self):
-        """测试广播数据."""
-        client_a, client_b = TestClient(w2g.app), TestClient(w2g.app)
-        s_data_a = {'msg': 'Hi!'}
-        s_data_b = {'text': 'Hello, World!'}
-        s_data_c = 'Not JSON!'
+    async def test_system_broadcast(self, client, manager):
+        """测试由系统发起广播."""
+        room_id = 'r0'
+        receiver_a_client_id, receiver_b_client_id = 2025, 2026
 
-        with (client_a.websocket_connect('/ws/r0/1001/') as websocket_a,
-              client_b.websocket_connect('/ws/r0/1002/') as websocket_b):
-            # 模拟由客户端a(实际上由服务器)发起广播, 且对自身广播.
-            await manager.broadcast(s_data_a, 'r0')
-            # 由客户端a发起广播.
-            websocket_a.send_json({
-                'props': {'type': 'websocket.broadcast'},
-                'data': s_data_b
-            })
-            # 客户端a发送无法解析的JSON数据.
-            websocket_a.send_text(s_data_c)
+        data = {'text': 'Hello, World!'}
 
-            r_data_a = websocket_a.receive_json()
-            # 客户端b会收到两条数据.
-            r_data_b1 = websocket_b.receive_json()
-            r_data_b2 = websocket_b.receive_json()
+        with (
+            client.websocket_connect(f'/ws/{room_id}/{receiver_a_client_id}/') as receiver_a_websocket,  # noqa: E501
+            client.websocket_connect(f'/ws/{room_id}/{receiver_b_client_id}/') as receiver_b_websocket  # noqa: E501
+        ):
+            await manager.broadcast(data, room_id)
 
-            room = manager.room_connections.get('r0')
+            room = manager.room_connections[room_id]
 
             assert len(room) == 2
-            assert r_data_a == s_data_a
-            assert r_data_b1 == s_data_a
-            assert r_data_b2.get('data') == s_data_b
+            assert receiver_a_websocket.receive_json() == data
+            assert receiver_b_websocket.receive_json() == data
 
-    def test_unicast(self):
+    def test_unicast(self, client, manager):
         """测试单播数据."""
-        client_a, client_b = TestClient(w2g.app), TestClient(w2g.app)
-        s_data = {'msg': 'Hi!'}
+        room_id = 'r0'
+        sender_client_id, receiver_client_id = 2025, 2026
 
-        with (client_a.websocket_connect('/ws/r0/1001/') as websocket_a,
-              client_b.websocket_connect('/ws/r0/1002/') as websocket_b):
-            # 客户端a向客户端b发起单播.
-            websocket_a.send_json({
-                'props': {
-                    'type': 'websocket.unicast',
-                    'receivedClientID': 1002
-                },
-                'data': s_data
-            })
-            # 客户端a向不存在的客户端发起单播.
-            websocket_a.send_json({
+        data = {'text': 'Hello, World!'}
+
+        with (
+            client.websocket_connect(f'/ws/{room_id}/{sender_client_id}/') as sender_websocket,  # noqa: E501
+            client.websocket_connect(f'/ws/{room_id}/{receiver_client_id}/') as receiver_websocket  # noqa: E501
+        ):
+            room = manager.room_connections[room_id]
+
+            # 发送方客户端向不存在的客户端发起单播.
+            sender_websocket.send_json({
                 'props': {
                     'type': 'websocket.unicast',
                     'receivedClientID': 2023
                 },
-                'data': s_data
+                'data': data
+            })
+            # 发送方客户端向接收方发起单播.
+            sender_websocket.send_json({
+                'props': {
+                    'type': 'websocket.unicast',
+                    'receivedClientID': receiver_client_id
+                },
+                'data': data
             })
 
-            r_data = websocket_b.receive_json()
-
-            room = manager.room_connections.get('r0')
-
             assert len(room) == 2
-            assert r_data.get('data') == s_data
+            assert receiver_websocket.receive_json()['data'] == data
+
+    def test_closes_room_when_last_client_disconnects(self, client, manager):
+        """测试最后一个客户端断开时关闭房间."""
+        room_id, client_id = 'r0', 2026
+
+        with client.websocket_connect(f'/ws/{room_id}/{client_id}/'):
+            room = manager.room_connections[room_id]
+
+            assert len(room) == 1
+
+        assert room_id not in manager.room_connections
